1eea69e5e46e5e6dc5208910016c8be16528a9a8
txsing <txsing@gmail.com>
2013-10-07 12:51:01 +0000

    taskItem

---
+++ ToBeDone/src/Logic.java
@@ -0,0 +1,105 @@
+import java.util.Vector;
+
+public class Logic {
+	private static final String CREATED_FAIL_MESSAGE = "Creating new task failed.";
+	private static final String MARKED_AS_DONE="The task is marked as done.";
+	private static final String MARKED_AS_UNDONE="The task is marked as undone.";
+	private static final String CREATED_SUCCESS_MESSAGE = "New task has been created.";
+	private static final String DELETE_SUCCESS_MESSAGE="The task is deleted successfully.";
+	private static final String UPDATE_SUCCESS_MESSAGE="The task is updated successfully.";
+	private static final String INVALID_ITEM="Item not found.";
+	private static final String NO_SEARCH_RESULT="No such task item.";
+	private static Vector<TaskItem> aimTasks;
+
+	
+	static String createTask(String description, String startTime, String endTime, String priority){
+		try{
+			TaskItem createNewOne=new TaskItem(description, startTime, endTime, priority);
+			Storage.save(createNewOne);
+		}catch(Exception e){
+			return CREATED_FAIL_MESSAGE;
+		}
+		return CREATED_SUCCESS_MESSAGE;
+	}
+	
+	static String markTask(int index){
+		TaskItem toMark;
+		int TaskID=indexToTaskID(index);
+		try{
+			toMark=Storage.retrieve(TaskID);
+		}catch(Exception e){
+			return INVALID_ITEM;
+		}
+		toMark.setStatus(true);
+		return MARKED_AS_DONE; 
+	}
+	
+	static String unmarkTask(int index){
+		TaskItem toUnMark;
+		int TaskID=indexToTaskID(index);
+		try{
+			toUnMark=Storage.retrive(TaskID);
+		}catch(Exception e){
+			return INVALID_ITEM;
+		}
+		toUnMark.setStatus(false);
+		return MARKED_AS_UNDONE;
+	}
+	
+	static String deleteTask(int index){
+		TaskItem toDelete;
+		int TaskID=indexToTaskID(index);
+		try{
+			toDelete=Storage.retrive(TaskID);
+		}catch(Exception e){
+			return INVALID_ITEM;
+		}
+		Storage.remove(toDelete);
+		return DELETE_SUCCESS_MESSAGE;
+	}
+	
+	static String updateTask(int index, String description, String startTime, String endTime, String priority ){
+		TaskItem toUpdate;
+		int TaskID=indexToTaskID(index);
+		try{
+			toUpdate=Storage.retrive(TaskID);
+		}catch(Exception e){
+			return INVALID_ITEM;
+		}
+		if(description!=null){
+			toUpdate.setTaskDescription(description);
+		}
+		if(startTime!=null){
+			toUpdate.setStartTime(startTime);
+		}
+		if(endTime!=null){
+			toUpdate.setEndTime(endTime);
+		}
+		if(priority!=null){
+			toUpdate.setPriority(priority);
+		}
+		return UPDATE_SUCCESS_MESSAGE;
+	}
+
+	static String searchTask(String keyword){
+		String searchResult = "Search results are as follow:\n";
+		String taskItemString;
+
+		aimTasks = Storage.search(keyword);
+		
+		if(aimTasks.capacity() == 0){
+			return NO_SEARCH_RESULT;
+		}
+		
+		for(int i = 0; i<aimTasks.capacity(); i++){
+			taskItemString = (i+1)+"."+aimTasks.get(i).toString() + "\\n";
+			searchResult.concat(taskItemString);
+		}
+		return searchResult;
+	}
+	
+	static int indexToTaskID(int index){
+		TaskItem temp=TobeDoneUI.getItem(index);		
+		return temp.getID();
+	}
+}
---
+++ ToBeDone/src/Storage.java
@@ -0,0 +1,90 @@
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.Scanner;
+import java.util.Vector;
+ 
+public class Storage {
+	private File file;
+	private Vector<TaskItem> tasks;
+
+	public Storage(String fileName) {
+		file = new File(fileName);
+		tasks = getTasksFromFile();
+	}
+
+	private Vector<TaskItem> getTasksFromFile() {
+		Vector<TaskItem> fileTasks = new Vector<TaskItem>();
+		try {
+			Scanner in = new Scanner(file);
+			while (in.hasNextLine()) {
+				String storageFormatOfTask = in.nextLine();
+				TaskItem fileTask = storageFormatToTaskItem(storageFormatOfTask);
+				fileTasks.add(fileTask);
+			}
+			in.close();
+		} catch (IOException e) {
+			System.err.println("");
+		}
+		return fileTasks;
+	}
+
+	private void writeTasksToFile(Vector<TaskItem> tasks) {
+		try {
+			FileWriter fstream = new FileWriter(file);
+			BufferedWriter out = new BufferedWriter(fstream);
+			for (TaskItem task : tasks) {
+				String storageFormatOfTask = taskItemToStorageFormat(task);
+				out.write(storageFormatOfTask);
+			}
+
+			out.close();
+		} catch (IOException e) {
+			System.err.println("");
+		}
+	}
+
+	public void store(TaskItem task) {
+		tasks.add(task);
+		writeTasksToFile(tasks);
+	}
+
+	public void store(int taskIndex, TaskItem task) {
+		tasks.add(taskIndex, task);
+		writeTasksToFile(tasks);
+	}
+
+	public TaskItem retrieve(int taskIndex) {
+		TaskItem targetTask = tasks.get(taskIndex);
+		return targetTask;
+	}
+
+	public Vector<TaskItem> retrieveAll() {
+		return tasks;
+	}
+
+	public TaskItem delete(int taskIndex) {
+		TaskItem deletedTask = tasks.remove(taskIndex);
+		writeTasksToFile(tasks);
+		return deletedTask;
+	}
+
+	private String taskItemToStorageFormat(TaskItem task) {
+		String storageFormat = "\"" + task.getDescription() + "\"";
+		return storageFormat;
+	}
+
+	private TaskItem storageFormatToTaskItem(String storageFormat) {
+		TaskItem task = new TaskItem();
+		String description = storageFormat.substring(1,
+				storageFormat.lastIndexOf('\"'));
+		task.setDescription(description);
+		return task;
+	}
+
+	public void clear() {
+		tasks.clear();
+		writeTasksToFile(tasks);
+	}
+}
