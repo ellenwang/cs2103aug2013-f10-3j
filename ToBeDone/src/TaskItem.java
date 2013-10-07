2bbed2ce31b4e1ea341208287a46ae06c1f3c3e9
txsing <txsing@gmail.com>
2013-10-07 12:51:01 +0000

    taskItem

--- ToBeDone/src/TaskItem.java
+++ ToBeDone/src/TaskItem.java
@@ -1,23 +1,73 @@
+import java.util.Date;
+
 
 public class TaskItem {
 	private int taskID;
-	private String taskDescription;
+	private String description;
 	private boolean valid;
+	private Date startTime;
+	private Date endTime;
+	private int priority;
 	
-	void setTaskDescription(String taskDescription) {
-		this.taskDescription = taskDescription;
+	TaskItem() {
+		
 	}
 	
-	void setTaskID(int taskID) {
+	TaskItem(String description, Date startTime, Date endTime, int priority) {
+		description = this.description;
+		startTime = this.startTime;
+		endTime = this.endTime;
+		priority = this.priority;
+	}
+	
+	public void setDescription(String taskDescription) {
+		this.description = taskDescription;
+	}
+	
+	public void setTaskID(int taskID) {
 		this.taskID = taskID;
 	}
 	
-	String getTaskDescription() {
-		return taskDescription;
+	public boolean isValid() {
+		return valid;
+	}
+
+	public void setValid(boolean valid) {
+		this.valid = valid;
+	}
+
+	public Date getStartTime() {
+		return startTime;
+	}
+
+	public void setStartTime(Date startTime) {
+		this.startTime = startTime;
+	}
+
+	public Date getEndTime() {
+		return endTime;
+	}
+
+	public void setEndTime(Date endTime) {
+		this.endTime = endTime;
+	}
+
+	String getDescription() {
+		return description;
 	}
 	
 	int getTaskID() {
 		return taskID;
 	}
+
+	public int getPriority() {
+		return priority;
+	}
+
+	public void setPriority(int priority) {
+		this.priority = priority;
+	}
+	
 	
 }
+
