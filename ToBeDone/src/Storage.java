49af5326f120c0eac70a2afd5931cb0a6781dbdf
Jesper Andersson (jesper andersson_89 <Jesper Andersson (jesper.andersson_89@hotmail.com)>
2013-10-09 01:43:46 +0800

    Storage methods completed First version of undo Changed toString() in TaskItem Fixed bug in search in logic

--- ToBeDone/src/Storage.java
+++ ToBeDone/src/Storage.java
@@ -2,14 +2,19 @@ import java.io.BufferedWriter;
 import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
-import java.sql.Date;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 import java.util.Calendar;
 import java.util.Scanner;
 import java.util.Vector;
 
 public class Storage {
+
+	private static SimpleDateFormat simpleDateFormat = new SimpleDateFormat(
+			"MM-dd'at'HH:mm");
+
 	private static String defaultFileName = "ToBeDone.txt";
-	
+
 	private static File file = new File(defaultFileName);
 	private static Vector<TaskItem> tasks = getTasksFromFile();
 
@@ -35,7 +40,7 @@ public class Storage {
 			BufferedWriter out = new BufferedWriter(fstream);
 			for (TaskItem task : tasks) {
 				String storageFormatOfTask = taskItemToStorageFormat(task);
-				out.write(storageFormatOfTask);
+				out.write(storageFormatOfTask + "\n");
 			}
 
 			out.close();
@@ -43,36 +48,37 @@ public class Storage {
 			System.err.println("");
 		}
 	}
-	
-	
-	private static void updateTaskId(Vector<TaskItem> tasks) {
+
+	private static void updateTaskIDs(Vector<TaskItem> tasks) {
 		for (int i = 0; i < tasks.size(); i++) {
 			tasks.get(i).setTaskID(i);
 		}
 	}
 
-	public static void store(TaskItem task) {
+	public static int store(TaskItem task) {
 		tasks.add(task);
-		updateTaskId(tasks);
+		updateTaskIDs(tasks);
 		writeTasksToFile(tasks);
+		return task.getTaskID();
 	}
 
-	public static void store(int taskIndex, TaskItem task) {
+	public static int store(int taskIndex, TaskItem task) {
 		tasks.add(taskIndex, task);
-		updateTaskId(tasks);
+		updateTaskIDs(tasks);
 		writeTasksToFile(tasks);
+		return task.getTaskID();
 	}
-	
+
 	public static Vector<TaskItem> search(String keyword) {
 		Vector<TaskItem> matchingTasks = new Vector<TaskItem>();
-		
+
 		for (TaskItem task : tasks) {
-			String taskDescription = task.getTaskDescription();
-			if (taskDescription.contains(keyword)) {
+			String taskInfo = task.toString();
+			if (taskInfo.contains(keyword)) {
 				matchingTasks.add(task);
 			}
 		}
-		
+
 		return matchingTasks;
 	}
 
@@ -84,46 +90,98 @@ public class Storage {
 	public static Vector<TaskItem> retrieveAll() {
 		return tasks;
 	}
-	
-	public static TaskItem delete(int taskIndex) {
+
+	public static Vector<TaskItem> retrieveUnfinished() {
 		Vector<TaskItem> finishedTask = new Vector<TaskItem>();
-		Date currentDate = (Date) Calendar.getInstance().getTime();
-		for (int i=0; i<tasks.size(); i++){
-			if(tasks.get(i).getEndTime().after(currentDate)){
+
+		for (int i = 0; i < tasks.size(); i++) {
+			TaskItem current = tasks.get(i);
+			current.updateStatus();
+			int curstatus = current.getStatus();
+			if (curstatus == 1) {
 				finishedTask.add(tasks.get(i));
 			}
 		}
 		return finishedTask;
 	}
 
-	public static Vector<TaskItem> retrieveUnfinished(){
+	public static Vector<TaskItem> retrieveFinished() {
 		Vector<TaskItem> finishedTask = new Vector<TaskItem>();
-		Date currentDate = (Date) Calendar.getInstance().getTime();
-		for (int i=0; i<tasks.size(); i++){
-			if(tasks.get(i).getEndTime().before(currentDate)){
+		for (int i = 0; i < tasks.size(); i++) {
+			TaskItem current = tasks.get(i);
+			current.updateStatus();
+			int curstatus = current.getStatus();
+			if (curstatus == 2) {
 				finishedTask.add(tasks.get(i));
 			}
 		}
 		return finishedTask;
 	}
+
+	public static Vector<TaskItem> retrieveExpired() {
+		Vector<TaskItem> finishedTask = new Vector<TaskItem>();
+		Date currentDate = (Date) Calendar.getInstance().getTime();
+		for (int i = 0; i < tasks.size(); i++) {
+			if (tasks.get(i).getEndTime().after(currentDate)) {
+				finishedTask.add(tasks.get(i));
+			}
+		}
+		return finishedTask;
+	}
+
 	public static TaskItem delete(int taskIndex) {
 		TaskItem deletedTask = tasks.remove(taskIndex);
-		updateTaskId(tasks);
+		updateTaskIDs(tasks);
 		writeTasksToFile(tasks);
 		return deletedTask;
 	}
 
 	private static String taskItemToStorageFormat(TaskItem task) {
-		String storageFormat = "\"" + task.getTaskDescription() + "\"";
+		String description = task.getDescription();
+		Date startTime = task.getStartTime();
+		String startTimeFormatted = simpleDateFormat.format(startTime);
+		Date endTime = task.getEndTime();
+		String endTimeFormatted = simpleDateFormat.format(endTime);
+		int priority = task.getPriority();
+		String separator = ";";
+
+		String storageFormat = "\"" + description + "\"" + startTimeFormatted + separator
+				+ endTimeFormatted + separator + priority;
+
 		return storageFormat;
 	}
 
 	private static TaskItem storageFormatToTaskItem(String storageFormat) {
-		TaskItem task = new TaskItem();
-		String description = storageFormat.substring(1,
-				storageFormat.lastIndexOf('\"'));
-		task.setTaskDescription(description);
+		String description = extractDescription(storageFormat);
+		String storageFormatWithoutDescription = removeDescription(storageFormat);
+		String[] taskInformation = storageFormatWithoutDescription.split(";");
+		Date startTime = null;
+		Date endTime = null;
+		try {
+			startTime = simpleDateFormat.parse(taskInformation[0]);
+			endTime = simpleDateFormat.parse(taskInformation[1]);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		int priority = Integer.parseInt(taskInformation[2]);
+
+		TaskItem task = new TaskItem(description, startTime, endTime, priority);
+		
 		return task;
+	}
+
+	private static String extractDescription(String storageFormat) {
+		int indexOfEndOfDescription = storageFormat.lastIndexOf('\"');
+		String description = storageFormat
+				.substring(1, indexOfEndOfDescription);
+		return description;
+	}
+
+	private static String removeDescription(String storageFormat) {
+		int indexOfEndOfDescription = storageFormat.lastIndexOf('\"');
+		String storageFormatWithoutDescription = storageFormat
+				.substring(indexOfEndOfDescription+1);
+		return storageFormatWithoutDescription;
 	}
 
 	public static void clear() {
