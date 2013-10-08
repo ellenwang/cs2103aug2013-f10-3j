d76b326a5886933bde0ed9c73cad20e3cd49c87f
Jesper Andersson (jesper andersson_89 <Jesper Andersson (jesper.andersson_89@hotmail.com)>
2013-10-09 01:43:46 +0800

    Storage methods completed First version of undo Changed toString() in TaskItem Fixed bug in search in logic

--- ToBeDone/src/TaskItem.java
+++ ToBeDone/src/TaskItem.java
@@ -1,39 +1,68 @@
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
 import java.util.Date;
 
-
 public class TaskItem {
+	private static SimpleDateFormat simpleDateFormat = new SimpleDateFormat(
+			"MM-dd'at'HH:mm");
+
 	private int taskID;
 	private String description;
 	private boolean valid;
-	private Date startTime=null;
-	private Date endTime=null;
+	private Date startTime;
+	private Date endTime;
 	private int priority;
-	
+	private STATUS status;
+
+	static enum STATUS {
+		finished, unfinished, expired
+	};
+
 	TaskItem() {
-		
+
 	}
-	
+
 	TaskItem(String description, Date startTime, Date endTime, int priority) {
-		description = this.description;
-		startTime = this.startTime;
-		endTime = this.endTime;
-		priority = this.priority;
+		this.description = description;
+		this.startTime = startTime;
+		this.endTime = endTime;
+		this.priority = priority;
+		status = STATUS.unfinished;
 	}
-	
+
 	public void setDescription(String taskDescription) {
 		this.description = taskDescription;
 	}
-	
+
 	public void setTaskID(int taskID) {
 		this.taskID = taskID;
 	}
-	
-	public boolean isValid() {
-		return valid;
+
+	public int getStatus() {
+		switch (status) {
+		case unfinished:
+			return 1;
+		case finished:
+			return 2;
+		case expired:
+			return 3;
+		default:
+			return -1;
+		}
 	}
 
-	public void setValid(boolean valid) {
-		this.valid = valid;
+	public void setStatus(int p) {
+		switch (p) {
+		case 1:
+			status = STATUS.unfinished;
+			break;
+		case 2:
+			status = STATUS.finished;
+			break;
+		case 3:
+			status = STATUS.expired;
+			break;
+		}
 	}
 
 	public Date getStartTime() {
@@ -55,7 +84,7 @@ public class TaskItem {
 	String getDescription() {
 		return description;
 	}
-	
+
 	int getTaskID() {
 		return taskID;
 	}
@@ -67,16 +96,26 @@ public class TaskItem {
 	public void setPriority(int priority) {
 		this.priority = priority;
 	}
-	public String toString(){
+
+	public void updateStatus() {
+		Date currentDate = (Date) Calendar.getInstance().getTime();
+		if (endTime.before(currentDate)) {
+			this.setStatus(3);
+		}
+
+	}
+
+	public String toString() {
 		String result = "";
-		if (getStartTime()!=null) {
-			result = "The task: "+getDescription()+"\t starts from: "+getStartTime()+"\t ends at: "+getEndTime()
-				+"\t priority: "+getPriority();
+		if (getStartTime() != null) {
+			result = "The task: " + getDescription() + "\t starts from: "
+					+ simpleDateFormat.format(getStartTime()) + "\t ends at: "
+					+ simpleDateFormat.format(getEndTime()) + "\t priority: "
+					+ getPriority();
 		} else {
-			result = "The task: "+getDescription()+"\t deadline: "+getEndTime()
-					+"\t priority: "+getPriority();
+			result = "The task: " + getDescription() + "\t deadline: "
+					+ getEndTime() + "\t priority: " + getPriority();
 		}
 		return result;
 	}
 }
-
--- ToBeDone/src/TobeDoneUI.java
+++ ToBeDone/src/TobeDoneUI.java
@@ -3,47 +3,55 @@ import java.text.SimpleDateFormat;
 import java.util.Date;
 import java.util.Scanner;
 
-
 public class TobeDoneUI {
-	
+
 	private static final String WELCOME_MESSAGE = "Now is ToBeDone moment";
 	private static final String NO_SUCH_COMMAND = "No such command!";
 	private static final String WRONG_COMMAND_FORMAT = "Please follow the correct command format!";
 	private static final String WRONG_TIME_FORMAT = "Please follow the time format as indicated!";
 	private static final String WRONG_TASK_DESCRIPTION_FORMAT = "Please double quote the task description!";
 	private static final String ENDTIME_SMALLER_THAN_STARTTIME = "The end time of tasks can't be smaller than start time";
+	private static final String WRONG_REDO = "a redo command must follow an undo command!";
+	private static final String MEANINGLESS_UNDO = "Meaningless to undo Search/View";
+
+	private static final int FLOATING_TASK = 1;
+	private static final int DEADLINE_TASK = 2;
+	private static final int TIMED_TASK = 3;
+
 	private static final int TASK_PRIORITY_HIGH = 3;
 	private static final int TASK_PRIORITY_MEDIUM = 2;
 	private static final int TASK_PRIORITY_LOW = 1;
 
-	
+	private static String lastCommandString = null;
+
+	static SimpleDateFormat simpleDateFormat = new SimpleDateFormat(
+			"MM-dd'at'HH:mm");
+
 	public static void main(String[] args) {
-		System.out.println(WELCOME_MESSAGE);
-		
-		readCommand();	
+		showToUser(WELCOME_MESSAGE);
+		readCommand();
 	}
-	
-	
-	static void showToUser(String message){
+
+	static void showToUser(String message) {
 		System.out.println(message);
 	}
-	
-	
-	
-	 static void readCommand() {
+
+	static void readCommand() {
 		boolean isExit = false;
 		Scanner scanner = new Scanner(System.in);
-		
-		while(!isExit){
+
+		while (!isExit) {
 			if (scanner.hasNext()) {
 				String comString = scanner.nextLine();
 				commandParser(comString);
+				if (comString != "undo") {
+					lastCommandString = comString;
+				}
 			}
 		}
-		scanner.close();	
+		scanner.close();
 	}
-	
-	 
+
 	static void commandParser(String comString) {
 		String comType;
 		String comPara;
@@ -66,56 +74,66 @@ public class TobeDoneUI {
 			break;
 		case "undo":
 			analyseUndo(comPara);
-		case "redo":
-			analyseRedo(comPara);
+			break;
+		case "finish":
+			analyseFinish(comPara);
+			break;
+		case "redo": {
+			if (lastCommandString != "undo") {
+				showToUser(WRONG_REDO);
+				break;
+			} else {
+				analyseRedo(comPara);
+			}
+		}
+			break;
 		default:
 			showToUser(NO_SUCH_COMMAND);
 		}
 	}
-	
-	static String getComType(String comString){
+
+	static String getComType(String comString) {
 		String comType;
-		
+
 		int index = 0;
 		int length = comString.length();
-		
+
 		while (index < length && comString.charAt(index) != ' ') {
 			index++;
 		}
 		// divide at the first blank space
 		comType = comString.substring(0, index);
-		
+
 		return comType;
 	}
-	
-	
-	static String getComPara(String comString){
+
+	static String getComPara(String comString) {
 		String comPara;
-		
+
 		int index = 0;
 		int length = comString.length();
-		
+
 		while (index < length && comString.charAt(index) != ' ') {
 			index++;
 		}
 		// divide at the first blank space
-		comPara = comString.substring(index+1);
-		
+		comPara = comString.substring(index + 1);
+
 		return comPara;
 	}
-	
-	
+
 	static void analyseCreate(String comPara) {
-		SimpleDateFormat simpleDateFormat = new SimpleDateFormat("MM-dd'at'HH:mm");
+
 		String userMessage;
-		
+
 		try {
 			String taskDescription = null;
 			Date taskStartTime = null;
 			Date taskEndTime = null;
 			int priority = TASK_PRIORITY_LOW;
 
-			// the location of the last right quotation, which means the end of take description
+			// the location of the last right quotation, which means the end of
+			// take description
 			int indexOfDes = 0;
 
 			for (int i = 0; i < comPara.length(); i++) {
@@ -129,26 +147,22 @@ public class TobeDoneUI {
 
 			taskDescription = comPara.substring(1, indexOfDes);
 
-			// taskParasExceptDes contains taskStartTime/taskEndTime and priority except description
+			// taskParasExceptDes contains taskStartTime/taskEndTime and
+			// priority except description
 			String taskStringExceptDes = comPara.substring(indexOfDes + 2);
 			String[] taskParasExceptDes = taskStringExceptDes.split(" ");
 
-			// since there are 3 kind of task. each has different number of parameters
-			int numOfPara = taskParasExceptDes.length;
+			// since there are 3 kind of task. each has different number of
+			// parameters
+			int taskType = taskParasExceptDes.length;
 
-			// a create command has 3 parameters at most
-			if(numOfPara > 3){
-				showToUser(WRONG_COMMAND_FORMAT);
-				return;
-			}
-			
 			// a floating task, just has description and priority
-			if (numOfPara == 1) {
+			if (taskType == FLOATING_TASK) {
 				priority = Integer.parseInt(taskParasExceptDes[0]);
 			}
 
 			// a task just has description\endTime and priority
-			if (numOfPara == 2) {
+			if (taskType == DEADLINE_TASK) {
 				try {
 					taskEndTime = simpleDateFormat.parse(taskParasExceptDes[0]);
 				} catch (ParseException e) {
@@ -159,34 +173,41 @@ public class TobeDoneUI {
 			}
 
 			// a full task with description\startTime\endTime and priority
-			if (numOfPara == 3) {
+			if (taskType == TIMED_TASK) {
 				try {
-					taskStartTime = simpleDateFormat.parse(taskParasExceptDes[0]);
+					taskStartTime = simpleDateFormat
+							.parse(taskParasExceptDes[0]);
 					taskEndTime = simpleDateFormat.parse(taskParasExceptDes[1]);
 				} catch (ParseException e) {
 					showToUser(WRONG_TIME_FORMAT);
 					return;
 				}
-				
-				if(taskEndTime.before(taskStartTime)){
+
+				if (taskEndTime.before(taskStartTime)) {
 					showToUser(ENDTIME_SMALLER_THAN_STARTTIME);
 					return;
 				}
-				
+
 				priority = Integer.parseInt(taskParasExceptDes[2]);
 			}
-			
-			userMessage = Logic.createTask(taskDescription, taskStartTime, taskEndTime, priority);
+
+			// a create command has 3 parameters at most
+			if (taskType > TIMED_TASK) {
+				showToUser(WRONG_COMMAND_FORMAT);
+				return;
+			}
+
+			userMessage = Logic.createTask(taskDescription, taskStartTime,
+					taskEndTime, priority);
 			showToUser(userMessage);
-			
+
 		} catch (Exception e) {
 			showToUser(WRONG_COMMAND_FORMAT);
 			return;
 		}
 	}
 
-	
-	static void analyseDelete(String comPara){
+	static void analyseDelete(String comPara) {
 		String useMessage;
 		try {
 			int taskIndex;
@@ -196,15 +217,14 @@ public class TobeDoneUI {
 		} catch (Exception e) {
 			showToUser(WRONG_COMMAND_FORMAT);
 		}
-		
+
 	}
 
+	static void analyseView(String comPara) {
+		String viewResult = null;
 
-	static void analyseView(String comPara){
-		String viewResult;
-		
-		//read a specified task
-		if(comPara.length()==1){
+		// read a specified task
+		if (comPara.length() == 1) {
 			try {
 				int taskIndex;
 				taskIndex = Integer.parseInt(comPara);
@@ -213,8 +233,8 @@ public class TobeDoneUI {
 				showToUser(WRONG_COMMAND_FORMAT);
 			}
 		}
-		
-		//read a list of task
+
+		// read a list of task
 		switch (comPara) {
 		case "all":
 			viewResult = Logic.viewAll();
@@ -229,44 +249,48 @@ public class TobeDoneUI {
 			showToUser(WRONG_COMMAND_FORMAT);
 			break;
 		}
-		
+
 		showToUser(viewResult);
 	}
 
-	
-	static void analyseSearch(String comPara){
+	static void analyseSearch(String comPara) {
 		String searchResult;
-		searchResult = Logic.SearchTask(comPara);
+		searchResult = Logic.searchTask(comPara);
 		showToUser(searchResult);
 	}
-	
-	
-	static void analyseUndo(String comPara){
-		String userMessage;
-		userMessage = Logic.undo(comPara);
+
+	static void analyseUndo(String comPara) {
+		String userMessage = null;
+		String lastComType = getComType(lastCommandString);
+
+		if (lastComType.equals("delete") || lastComType.equals("create")
+				|| lastComType.equals("finish")) {
+			userMessage = Logic.undo(lastComType);
+		} else {
+			showToUser(MEANINGLESS_UNDO);
+		}
+
 		showToUser(userMessage);
-		
 	}
-	
-	
-	static void analyseRedo(String comPara){
+
+	static void analyseRedo(String comPara) {
 		String userMessage;
-		userMessage = Logic.redo(comPara);
+		userMessage = Logic.redo(lastCommandString);
 		showToUser(userMessage);
 	}
-	
-	
 
+	static void analyseFinish(String comPara) {
 
-
-
-
-
-
-
-
-
+		// read a specified task
+		if (comPara.length() == 1) {
+			try {
+				int taskIndex;
+				taskIndex = Integer.parseInt(comPara);
+				Logic.markTask(taskIndex);
+			} catch (Exception e) {
+				showToUser(WRONG_COMMAND_FORMAT);
+			}
+		}
+	}
 
 }
-
-
