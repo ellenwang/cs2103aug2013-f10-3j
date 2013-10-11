5f16924981413f1cbc02be4e5536c20df283b5da
Jesper Andersson (jesper andersson_89 <Jesper Andersson (jesper.andersson_89@hotmail.com)>
2013-10-11 17:11:25 +0800

    Changed TaskItem.Status from int to enum in all places

--- ToBeDone/src/Storage.java
+++ ToBeDone/src/Storage.java
@@ -2,34 +2,34 @@ import java.io.BufferedWriter;
 import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
+import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 import java.util.Scanner;
 import java.util.Vector;
 
-
 public class Storage {
 	// messages to the user
 	private static final String MESSAGE_FAILED_TO_WRITE_TO_FILE = "Failed to write to file.";
 	private static final String MESSAGE_FAILED_TO_READ_FROM_FILE = "Failed to read from file.";
-	private static final String MESSAGE_PARSE_DATE_FAILED = "Failed to parse date.";
+	private static final String MESSAGE_FAILED_TO_PARSE_DATE = "Failed to parse date.";
 	private static final String FILE_NAME = "ToBeDone.txt";
-	
+
 	// file written to and read from
 	private static File file = new File(FILE_NAME);
 
 	private static SimpleDateFormat simpleDateFormat = new SimpleDateFormat(
 			"dd/MM','HH:mm");
-	
+
 	public static void store(Vector<TaskItem> taskItems) {
 		writeTasksToFile(taskItems);
 	}
-	
+
 	public static Vector<TaskItem> retrieve() {
 		Vector<TaskItem> allTasks = getTasksFromFile();
 		return allTasks;
 	}
-	
+
 	private static Vector<TaskItem> getTasksFromFile() {
 		Vector<TaskItem> fileTasks = new Vector<TaskItem>();
 		try {
@@ -42,6 +42,8 @@ public class Storage {
 			in.close();
 		} catch (IOException e) {
 			System.err.println(MESSAGE_FAILED_TO_READ_FROM_FILE);
+		} catch (ParseException e) {
+			System.err.println(MESSAGE_FAILED_TO_PARSE_DATE);
 		}
 		return fileTasks;
 	}
@@ -61,58 +63,66 @@ public class Storage {
 		}
 	}
 
-	
 	private static String taskItemToStorageFormat(TaskItem task) {
 		String description = task.getDescription();
 
 		Date startTime = task.getStartTime();
-		String startTimeFormatted;
-		if (startTime != null) {
-			startTimeFormatted = simpleDateFormat.format(startTime);
-		} else {
-			startTimeFormatted = "";
-		}
+		String formattedStartTime = formatDate(startTime);
 
 		Date endTime = task.getEndTime();
-		String endTimeFormatted;
-		if (endTime != null) {
-			endTimeFormatted = simpleDateFormat.format(endTime);
-		} else {
-			endTimeFormatted = "";
-		}
+		String formattedEndTime = formatDate(endTime);
 
 		int priority = task.getPriority();
-		String separator = ";";
+		TaskItem.Status status = task.getStatus();
 
-		String storageFormat = "\"" + description + "\"" + startTimeFormatted
-				+ separator + endTimeFormatted + separator + priority;
+		String storageFormat = "\"" + description + "\"" + formattedStartTime
+				+ ";" + formattedEndTime + ";" + priority + ";" + status;
 
 		return storageFormat;
 	}
-	
-	private static TaskItem storageFormatToTaskItem(String storageFormat) {
-		String description = extractDescription(storageFormat);
-		String storageFormatWithoutDescription = removeDescription(storageFormat);
-		String[] taskInformation = storageFormatWithoutDescription.split(";");
-		Date startTime = null;
-		Date endTime = null;
-		try {
-			if (!taskInformation[0].equals("")) {
-				startTime = simpleDateFormat.parse(taskInformation[0]);
-			}
 
-			if (!taskInformation[1].equals("")) {
-				endTime = simpleDateFormat.parse(taskInformation[1]);
-			}
-		} catch (Exception e) {
-			System.err.println(MESSAGE_PARSE_DATE_FAILED);
+	private static String formatDate(Date date) {
+		String formattedDate;
+		if (date != null) {
+			formattedDate = simpleDateFormat.format(date);
+		} else {
+			formattedDate = "";
 		}
-		int priority = Integer.parseInt(taskInformation[2]);
-		TaskItem task = new TaskItem(description, startTime, endTime, priority);
 
+		return formattedDate;
+	}
+
+	private static TaskItem storageFormatToTaskItem(String storageFormat)
+			throws ParseException {
+		String[] taskInformation = splitStorageFormat(storageFormat);
+
+		String description = taskInformation[0];
+		Date startTime = simpleDateFormat.parse(taskInformation[1]);
+		Date endTime = simpleDateFormat.parse(taskInformation[2]);
+		int priority = Integer.parseInt(taskInformation[3]);
+		TaskItem.Status status = TaskItem.Status.valueOf(taskInformation[4]);
+
+		TaskItem task = new TaskItem(description, startTime, endTime, priority,
+				status);
 		return task;
 	}
-	
+
+	private static String[] splitStorageFormat(String storageFormat) {
+		String description = extractDescription(storageFormat);
+		String storageFormatWithoutDescription = removeDescription(storageFormat);
+
+		String[] taskInformationExceptDescription = storageFormatWithoutDescription
+				.split(";");
+		String[] allTaskInformation = new String[taskInformationExceptDescription.length];
+
+		allTaskInformation[0] = description;
+		for (int i = 0; i < taskInformationExceptDescription.length; i++) {
+			allTaskInformation[i + 1] = taskInformationExceptDescription[i];
+		}
+
+		return allTaskInformation;
+	}
+
 	private static String extractDescription(String storageFormat) {
 		int indexOfEndOfDescription = storageFormat.lastIndexOf('\"');
 		String description = storageFormat
