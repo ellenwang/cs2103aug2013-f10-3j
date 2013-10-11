9c672191143f0b4a0afe22348283546ad489422c
txsing <txsing@gmail.com>
2013-10-11 11:33:24 +0000

    fixed view and update command

--- ToBeDone/src/Parser.java
+++ ToBeDone/src/Parser.java
@@ -1,9 +1,21 @@
+//import java.util.Scanner;
 import java.util.Vector;
 
-public class Parser {
-	// messages to the user
-	static final String WRONG_DESCRIPTION = "Wrong description format!";
 
+public class Parser {
+	static final String WRONG_DESCRIPTION = "Wrong description format!";
+	
+//	public static void main(String[] args) {
+//		Scanner scanner = new Scanner(System.in);
+//		String string = scanner.nextLine();
+//		try {
+//			parseCommand(string);
+//		} catch (Exception e) {
+//			// TODO Auto-generated catch block
+//			e.printStackTrace();
+//		}
+//	}
+	
 	public static Command parseCommand(String commandString) throws Exception {
 		String commandType = getComType(commandString);
 		try {
@@ -14,62 +26,72 @@ public class Parser {
 			throw e;
 		}
 	}
-
-	private static Vector<String> getComParas(String commandString)
-			throws Exception {
-		Vector<String> parameters = new Vector<String>();
+		
+	private static Vector<String> getComParas(String commandString) throws Exception {
+		Vector<String> parameters = new Vector<String>(); 
 		String comParaString = getComParaString(commandString);
-
-		if (comParaString == null) {
+		
+		if(comParaString == null){
 			parameters = null;
 			return parameters;
 		}
-
+		
+		//only for the update command, description is not the first parameter 
+		if(comParaString.contains("\"")&&comParaString.charAt(0)!='\"'){
+			parameters.add(comParaString.charAt(0)+"");
+			comParaString = comParaString.substring(2);
+		}
+		
 		String description = getDescription(comParaString);
-		if (description != null) {
+			
+		if(description != null){
+			if(description.equals(WRONG_DESCRIPTION)){
+				Exception exception = new Exception(WRONG_DESCRIPTION);
+				throw exception;
+			}
 			parameters.add(description);
 		}
-
-		if (description.equals(WRONG_DESCRIPTION)) {
-			Exception exception = new Exception(WRONG_DESCRIPTION);
-			throw exception;
-		}
-
+		
 		String[] parasExceptDes = getComParasExceptDes(comParaString);
 		for (int i = 0; i < parasExceptDes.length; i++) {
 			parameters.add(parasExceptDes[i]);
 		}
-
-		return parameters;
+		
+		return parameters;	
 	}
-
+	
 	protected static String[] getComParasExceptDes(String comParaString) {
 		int indexOfDes = comParaString.lastIndexOf('\"');
+		String[] parasArray;
 
 		// parasExceptDes contains StartTime/EndTime and
 		// priority except description
-		String parsWithoutDes = comParaString.substring(indexOfDes + 2);
-		String[] parasArray = parsWithoutDes.split(" ");
-
+		if (indexOfDes == -1) {
+			String parsWithoutDes = comParaString.substring(indexOfDes + 1);
+			parasArray = parsWithoutDes.split(" ");
+		} else {
+			String parsWithoutDes = comParaString.substring(indexOfDes + 2);
+			parasArray = parsWithoutDes.split(" ");
+		}
 		return parasArray;
 	}
-
+	
 	private static String getDescription(String comParaString) {
 		int indexOfDes = comParaString.lastIndexOf('\"');
 		if (indexOfDes == -1) {
 			return null;
 		}
-		if (indexOfDes == 0) {
+		if(indexOfDes == 0){
 			return WRONG_DESCRIPTION;
 		}
-		if (!comParaString.startsWith("\"")) {
+		if(!comParaString.startsWith("\"")){
 			return WRONG_DESCRIPTION;
 		}
-
-		String description = comParaString.substring(1, indexOfDes);
+		
+		String description = comParaString.substring(1,indexOfDes);
 		return description;
-	}
-
+	}	
+	
 	private static String getComParaString(String commandString) {
 		String comParaString = null;
 		int index = 0;
@@ -86,7 +108,7 @@ public class Parser {
 		comParaString = commandString.substring(index + 1);
 		return comParaString;
 	}
-
+	
 	private static String getComType(String commandString) {
 		String comType = null;
 		int index = 0;
