50e35d6a730f5cfa6fbe977fff7496a2918ad08d
Wang Zi <wangziellen@gmail.com>
2013-11-11 11:18:00 +0800

    ToDoListImp class refactored

--- ToBeDone/ToBeDone/src/main/java/com/tobedone/logic/ToDoListImp.java
+++ ToBeDone/ToBeDone/src/main/java/com/tobedone/logic/ToDoListImp.java
@@ -35,16 +35,24 @@ public class ToDoListImp extends ToDoList {
 	}
 
 	@Override
+	/**
+	 * Create a new task and store to the storage. Return true if succeeded, false otherwise.
+	 */
 	public boolean createTask(TaskItem newTask) {
 		logger.info(LogMessages.INFO_CREATE_TASK);
+		
 		allTasks.add(newTask);
 		return storage.store(allTasks);
 	}
 
 	@Override
+	/**
+	 * Update a task in the current task list by index. Return the original task.
+	 */
 	public TaskItem updateTask(int index, TaskItem updatedTask)
 			throws TaskNotExistException, IOException {
 		logger.info(LogMessages.INFO_UPDATE_TASK);
+		
 		TaskItem oldTask = matchingTasks.get(index);
 		setLastUpdatedTask(oldTask);
 		matchingTasks.remove(oldTask);
@@ -57,19 +65,28 @@ public class ToDoListImp extends ToDoList {
 	}
 
 	@Override
+	/**
+	 * Delete a task in the current task list by index. Return the removed task.
+	 */
 	public TaskItem deleteTaskById(int index) throws TaskNotExistException,
 			IOException {
-		logger.info(LogMessages.INFO_DELETE_TASK_BYID);
-		TaskItem task = matchingTasks.remove(index);
-		deleteTask(task);
-		return task;
+		logger.info(LogMessages.INFO_DELETE_TASK_BY_ID);
+		
+		TaskItem deletedTask = matchingTasks.remove(index);
+		deleteTask(deletedTask);
+		return deletedTask;
 	}
-
+	
+	@Override
+	/**
+	 * Delete the exact task object in the current task list. Return the removed task.
+	 */
 	public boolean deleteTask(TaskItem task) {
 		logger.info(LogMessages.INFO_DELETE_TASK);
-		boolean flag = allTasks.remove(task);
+		
+		boolean deletionSuccessful = allTasks.remove(task);
 		setLastDeletedTask(task);
 		storage.store(allTasks);
-		return flag;
+		return deletionSuccessful;
 	}
 }
--- ToBeDone/ToBeDone/src/main/java/com/tobedone/utils/LogMessages.java
+++ ToBeDone/ToBeDone/src/main/java/com/tobedone/utils/LogMessages.java
@@ -9,21 +9,26 @@ public class LogMessages {
 	public static final String ERROR_UPDATE_UNDO_FAILED = "Failed to undo update.";
 	// INFO
 	public static final String INFO_ADD_UNDO = "Undo the create command.";
+	public static final String INFO_CREATE_TASK =  "Create a new task.";
+	public static final String INFO_DELETE_TASK = "Delete a task.";
+	public static final String INFO_DELETE_TASK_BY_ID = "Delete a task by ID.";
 	public static final String INFO_POP_COMMAND = "Pop command from command stack.";
 	public static final String INFO_POP_UNDO_COMMAND = "Pop undone command from undo command stack.";
 	public static final String INFO_PUSH_COMMAND = "Push command into command stack.";
+	public static final String INFO_UPDATE_TASK = "Update a task.";
+	
 	public static final String INFO_SEARCH = "Executing search command.";
 	public static final String INFO_CLEAR = "Clear tasks.";
 	public static final Object INFO_EXIT = "Exit.";
 	public static final String INFO_HELP = "Help.";
 	public static final String INFO_DISPLAY_PRIORITY = "Display task list sorted by priority.";
 	public static final String INFO_COMPLETE = "Complete a task.";
-	public static final String INFO_CREATE_TASK =  "Create a new task.";
-	public static final String INFO_UPDATE_TASK = "Update a task.";
-	public static final String INFO_DELETE_TASK_BYID = "Delete a task by ID.";
+	
+	
+	
 	public static final String INFO_DELETE = "Delete a task by index.";
 	public static final String INFO_ADD = "Add a new task.";
-	public static final String INFO_DELETE_TASK = "Delete a task.";
+	
 	public static final String INFO_UNDO_ACTION = "Undo previous action";
 	public static final String INFO_FINISH = "Finish a task.";
 	public static final String INFO_REMOVE = "Remove a task.";
